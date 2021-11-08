7f89235232eccf697e1576aebe10f4b272f04986
Noca25 <valaskanora25@gmail.com>
2021-11-08 14:41:28 +0100

    html
    html

---
+++ getfit/src/app/personaldata/personaldata.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-personaldata',
+  templateUrl: './personaldata.component.html',
+  styleUrls: ['./personaldata.component.css']
+})
+export class PersonaldataComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
